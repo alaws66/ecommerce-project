47c697cce59b335e36fbee730950dc9f95408112
Aimee Laws <aimee.laws13@gmail.com>
2022-11-21 11:03:26 +0000

    show price, discount + low stock from user input

--- frontend/src/components/ProductDetails.jsx
+++ frontend/src/components/ProductDetails.jsx
@@ -1,18 +1,61 @@
 const ProductDetails = ({products}) => {
+  //***** variables *****/ 
   let sizeArr = [];
   let colourArr = [];
 
+  let currentSize = null;
+  let currentColour = null;
+  let currentStockItem = null;
+
+  //***** pushes one of every colour and size in seperate arrays  *****/ 
   products.stock.map((stock) => {
     if (!sizeArr.includes(stock.size)) {
-        sizeArr.push(stock.size)
+        sizeArr.push(stock.size);
     }
     
     if (!colourArr.includes(stock.colour)) {
-        colourArr.push(stock.colour)
+        colourArr.push(stock.colour);
     }
   });
 
+  //***** function to show correct price + discount from user selection *****/
+  function getStockEntry(size, colour) {
+    currentStockItem = `${size}, ${colour}`;
+
+    //***** get price and discount html tags *****/
+    const productPrice = document.getElementById('productPrice');
+    const productDiscount = document.getElementById('productDiscount');
+    const lowStock = document.getElementById('stockWarning');
+
+    //***** map through stock array *****/
+    products.stock.map((stock) => {
+      if (stock.size === size && stock.colour === colour && stock.discount) {
+        productPrice.textContent = `£${stock.price.toFixed(2)}`;
+
+        //***** calculate and show discount *****/
+        let discountPrice = (stock.discount / 100) * stock.price;
+        productDiscount.textContent = `-£${discountPrice.toFixed(2)}`;
+
+        //***** if discount doesn't exist show only price *****/
+      } else if (stock.size === size && stock.colour === colour) {
+        productPrice.textContent = `£${stock.price.toFixed(2)}`;
+        productDiscount.textContent = '';
+      }
+
+      if (stock.size == size && stock.colour === colour && stock.quantity <= 5) {
+        lowStock.textContent = `Only ${stock.quantity} items left!`;
+      } else if (stock.size == size && stock.colour === colour && stock.quantity > 5) {
+        lowStock.textContent = '';
+      }
+    })
+  }
+
+  //***** function to select size of product from stock *****/
   function changeSize(size) {
+    currentSize = size;
+
+    getStockEntry(currentSize, currentColour);
+
     products.stock.map((stock) => {
       if (stock.size === size) {
         document.getElementById(size).classList.add('active-btn');
@@ -25,22 +68,27 @@ const ProductDetails = ({products}) => {
       } else if (stock.size === size && stock.quantity !== 0) {
         document.getElementById(stock.colour).classList.remove('out-stock');
       }
-
-      // if (stock.size === size && stock.discount) {
-      //   console.log(stock.discount);
-      // }
     })
-    
   }
 
+  //***** function to select colour of product from stock *****/
   function changeColour(colour) {
+    currentColour = colour;
+
+    getStockEntry(currentSize, currentColour);
+
+    //***** adds images based on selected colour *****/
     products.image_collection.map((collect) => {
       if (collect.colour === colour) {
-        document.getElementById('product-img').src = collect.images[0];
+        document.getElementById('product-img-1').src = collect.images[0];
         document.getElementById(colour).classList.add('active-btn');
       } else if (collect.colour !== colour) {
         document.getElementById(collect.colour).classList.remove('active-btn');
       }
+
+      if (collect.colour === colour && collect.images[1]) {
+        document.getElementById('product-img-2').src = collect.images[1];
+      } 
     })
 
     products.stock.map((stock) => {
@@ -49,30 +97,22 @@ const ProductDetails = ({products}) => {
       } else if (stock.colour === colour && stock.quantity !== 0 ) {
         document.getElementById(stock.size).classList.remove('out-stock');
       }
-
-      // if (stock.colour === colour && stock.discount) {
-      //   console.log(stock.discount);
-      // }
     })
-    
   }
 
   return ( 
     <div>
         <div className="product-details">
           <div className="preview-images">
-            <img src="../../public/product-placeholder.png" id="product-img"></img>
+            <img src="../../public/product-placeholder.png" id="product-img-1" alt={products.title}></img>
+            <img src="../../public/product-placeholder.png" id="product-img-2" alt={products.title}></img>
           </div>
-          <div className="product-info">
+          <div className="full-product-info">
             <h1>{products.title}</h1>
-
-            {/* {products.stock.map(({ size, colour, price, discount }) => (
-              <div>
-                {discount && (
-                  <p>{discount}% off</p>
-                )}
-              </div>
-            ))} */}
+            <div className="product-prices">
+              <p id="productPrice">&pound; --.--</p>
+              <p id="productDiscount"></p>
+            </div>
 
             <p>{products.description}</p>
 
@@ -89,6 +129,8 @@ const ProductDetails = ({products}) => {
                     <button id={colour} onClick={() => changeColour(colour)}>{colour}</button>
                 ))}
             </div>
+
+            <p id="stockWarning"></p>
 
             <button className="add-btn">Add to Basket</button>
           </div>
