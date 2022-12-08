77f5d9cf54e2f4882bcb159cca3a751d8e608813
Aimee Laws <aimee.laws13@gmail.com>
2022-12-08 12:12:59 +0000

    Added navigation accessibility

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 const Home = () => {
   return ( 
     <div className="homepage">
@@ -16,18 +18,23 @@ const Home = () => {
       <div className="divider"></div>
 
       <div className="homepage-content">
-        <div className="display-product">
-          <h1>Clothing</h1>
-          <div className="display-img">
-            <img src="../../public/product-images/straight-denim-lightWash-front.jpg"></img>
+        <NavLink to="/clothing">
+          <div className="display-product">
+            <h1>Clothing</h1>
+            <div className="display-img">
+              <img src="../../public/product-images/straight-denim-lightWash-front.jpg"></img>
+            </div>
           </div>
-        </div>
-        <div className="display-product">
-          <h1>Accessories</h1>
-          <div className="display-img">
-            <img src="../../public/product-images/bucket-hat-green.jpg"></img>
+        </NavLink>
+        <NavLink to="/accessories">
+          <div className="display-product">
+            <h1>Accessories</h1>
+            <div className="display-img">
+              <img src="../../public/product-images/bucket-hat-green.jpg"></img>
+            </div>
           </div>
-        </div>
+        </NavLink>
+        
       </div>
     </div>
    );
